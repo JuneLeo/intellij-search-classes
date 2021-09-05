81c228cd095b6edc039917a5d9547cb436a50aaf
GitHub Action <action@github.com>
2021-09-05 05:58:42 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/juneleo/intellijsearchclasses/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.juneleo.intellijsearchclasses.services
+
+import com.github.juneleo.intellijsearchclasses.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
